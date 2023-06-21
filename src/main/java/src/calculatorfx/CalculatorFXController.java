f1a9eb18808690efeb6f4877ff21e5ac011e7f95
Baptiste Gogolewski <gogolewskibaptiste@gmail.com>
2023-06-21 16:46:03 +0200

    Style finish and start of calculation

--- src/main/java/src/calculatorfx/CalculatorFXController.java
+++ src/main/java/src/calculatorfx/CalculatorFXController.java
@@ -1,7 +1,9 @@
 package src.calculatorfx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -12,12 +14,46 @@ public class CalculatorFXController
     private Pane TitlePane;
 
     @FXML
-    private ImageView CloseButton;
+    private ImageView CloseButton, MinimizeButton;
 
     @FXML
-    private ImageView MinimizeButton;
+    private Label ResultLabel;
+
+    private double x, y;
+    private double Num1 = 0;
+    private String Operator = "+";
 
     public void Init(Stage primaryStage)
+    {
+        // Permet de faire bouger la fenêtre
+        TitlePane.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        TitlePane.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - x);
+            primaryStage.setY(event.getScreenY() - y);
+        });
+
+        // Ferme la fenêtre
+        CloseButton.setOnMouseClicked(mouseEvent -> {
+            primaryStage.close();
+        });
+
+        // Minimise la fenêtre
+        MinimizeButton.setOnMouseClicked(mouseEvent -> {
+            primaryStage.setIconified(true);
+        });
+    }
+
+    @FXML
+    private void OnZeroClick(MouseEvent event)
+    {
+        ResultLabel.setText(String.valueOf(0));
+    }
+
+    @FXML
+    private void OnSymbolClick(MouseEvent event)
     {
 
     }
