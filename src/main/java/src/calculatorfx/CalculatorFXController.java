14d3a8d7b26aa49301a6d07333cfb3998142b7aa
Baptiste Gogolewski <gogolewskibaptiste@gmail.com>
2023-06-21 22:05:04 +0200

    Calculator is finish

--- src/main/java/src/calculatorfx/CalculatorFXController.java
+++ src/main/java/src/calculatorfx/CalculatorFXController.java
@@ -9,7 +9,6 @@ import javafx.stage.Stage;
 
 public class CalculatorFXController
 {
-
     @FXML
     private Pane TitlePane;
 
@@ -19,7 +18,13 @@ public class CalculatorFXController
     @FXML
     private Label ResultLabel;
 
-    private double x, y;
+    private String Operator = "";
+
+    // Permet de savoir si un operateur est selectionné
+    private boolean OperatorSelected;
+    private boolean UpdateScreen;
+
+    private double x, y, Calcul;
 
     public void Init(Stage primaryStage)
     {
@@ -45,14 +50,162 @@ public class CalculatorFXController
     }
 
     @FXML
-    private void OnZeroClick(MouseEvent event)
+    private void CalculResult()
     {
-        ResultLabel.setText(String.valueOf(0));
+        switch (Operator)
+        {
+            case "+":
+                Calcul = Calcul + Double.valueOf(ResultLabel.getText());
+                break;
+
+            case "-":
+                Calcul = Calcul - Double.valueOf(ResultLabel.getText());
+                break;
+
+            case "*":
+                Calcul = Calcul * Double.valueOf(ResultLabel.getText());
+                break;
+
+            case "/":
+                Calcul = Calcul / Double.valueOf(ResultLabel.getText());
+                break;
+
+            default:
+                break;
+        }
+        Operator = "";
+        OperatorSelected = false;
+        ResultLabel.setText(Double.toString(Calcul));
+    }
+
+    private void AddNumber(int number)
+    {
+        String str = String.valueOf(number);
+        if (UpdateScreen)
+            UpdateScreen = false;
+        else
+        {
+            if (!(ResultLabel.getText().equals("0")))
+                str = ResultLabel.getText() + str;
+        }
+        ResultLabel.setText(str);
+    }
+
+    private void ClickOnOperator(String operator)
+    {
+        System.out.println(operator);
+        if (OperatorSelected)
+        {
+            CalculResult();
+            ResultLabel.setText(Double.toString(Calcul));
+            OperatorSelected = false;
+            Operator = operator;
+            UpdateScreen = true;
+        }
+        else
+        {
+            OperatorSelected = true;
+            UpdateScreen = true;
+            Operator = operator;
+            Calcul = Double.valueOf(ResultLabel.getText());
+        }
     }
 
     @FXML
-    private void OnSymbolClick(MouseEvent event)
+    private void OnZeroClick(MouseEvent event)
     {
+        AddNumber(0);
+    }
 
+    @FXML
+    private void OnOneClick(MouseEvent event)
+    {
+        AddNumber(1);
+    }
+
+    @FXML
+    private void OnTwoClick(MouseEvent event)
+    {
+        AddNumber(2);
+    }
+
+    @FXML
+    private void OnThreeClick(MouseEvent event)
+    {
+        AddNumber(3);
+    }
+
+    @FXML
+    private void OnFourClick(MouseEvent event)
+    {
+        AddNumber(4);
+    }
+
+    @FXML
+    private void OnFiveClick(MouseEvent event)
+    {
+        AddNumber(5);
+    }
+
+    @FXML
+    private void OnSixClick(MouseEvent event)
+    {
+        AddNumber(6);
+    }
+
+    @FXML
+    private void OnSevenClick(MouseEvent event)
+    {
+        AddNumber(7);
+    }
+
+    @FXML
+    private void OnHeightClick(MouseEvent event)
+    {
+        AddNumber(8);
+    }
+
+    @FXML
+    private void OnNineClick(MouseEvent event)
+    {
+        AddNumber(9);
+    }
+
+    @FXML
+    private void OnDivideClick(MouseEvent event)
+    {
+        ClickOnOperator("/");
+    }
+
+    @FXML
+    private void OnMultiplyClick(MouseEvent event)
+    {
+        ClickOnOperator("*");
+    }
+
+    @FXML
+    private void OnPlusClick(MouseEvent event)
+    {
+        ClickOnOperator("+");
+    }
+
+    @FXML
+    private void OnMinusClick(MouseEvent event)
+    {
+        ClickOnOperator("-");
+    }
+
+    @FXML
+    private void OnDotClick(MouseEvent event)
+    {
+        ResultLabel.setText(ResultLabel.getText() + ".");
+    }
+
+    @FXML
+    private void OnClearClick(MouseEvent event)
+    {
+        ResultLabel.setText("0");
+        OperatorSelected = false;
+        UpdateScreen = false;
     }
 }
